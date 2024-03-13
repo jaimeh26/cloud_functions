92663c58033e734cc17adde4475fa82a3d7fe902
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-03-13 18:03:02 -0400

    Create maximun_number.py

---
+++ maximun_number.py
@@ -0,0 +1,10 @@
+# Create a Cloud Function that finds the maximum number in a list(the list can be initialized inside the cloud function) this is the list-->[4434,1233,7987,4567,3123,4356,9808,1211,7888,9328]
+
+def find_max_number(request):
+    # List of numbers
+    numbers = [4434, 1233, 7987, 4567, 3123, 4356, 9808, 1211, 7888, 9328]
+
+    # Find maximum number
+    max_number = max(numbers)
+
+    return {'max_number': max_number}
