9da8428f3f50cb30395cd8099f0567718761abd6
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-03-13 18:59:23 -0400

    Create longest_word.py

---
+++ longest_word.py
@@ -0,0 +1,10 @@
+# Create a cloud function that finds the longest word out of the following list : ["abracadabra","pineapple","food","Daisy","mcit","Montrealcollegeinformationandtechnology"]
+
+def find_longest_word(request):
+    # List of words
+    words = ["abracadabra", "pineapple", "food", "Daisy", "mcit", "Montrealcollegeinformationandtechnology"]
+
+    # Find the longest word
+    longest_word = max(words, key=len)
+
+    return {'longest_word': longest_word}
