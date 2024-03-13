e5db6a9af97da91d97e11ddf23206580bffff22a
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-03-13 18:13:20 -0400

    Create sort_ascending.py

---
+++ sort_ascending.py
@@ -0,0 +1,10 @@
+# Create cloud function that sorts the list in ascending order [4434,1233,7987,4567,3123,4356,9808,1211,7888,9328]
+
+def sort_list_ascending(request):
+    # List of numbers
+    numbers = [4434, 1233, 7987, 4567, 3123, 4356, 9808, 1211, 7888, 9328]
+
+    # Sort the list in ascending order
+    sorted_numbers = sorted(numbers)
+
+    return {'sorted_numbers': sorted_numbers}
