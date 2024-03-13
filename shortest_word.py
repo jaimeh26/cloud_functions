1bb216d7b8c75959309c57f2a6b9193d1f4a76a2
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-03-13 19:08:33 -0400

    Create shortest_word.py

---
+++ shortest_word.py
@@ -0,0 +1,10 @@
+# Create a cloud function that finds the shortest word out of the following list :["abracadabra","pineapple","food","Daisy","mcit","Montrealcollegeinformationandtechnology"]
+
+def find_shortest_word(request):
+    # List of words
+    words = ["abracadabra", "pineapple", "food", "Daisy", "mcit", "Montrealcollegeinformationandtechnology"]
+
+    # Find the shortest word
+    shortest_word = min(words, key=len)
+
+    return {'shortest_word': shortest_word}
